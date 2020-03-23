90888227ad5a8745cded4a2a251fdf557ce467f2
Miguel Fernandes <miguelmanofernandes@gmail.com>
2020-03-23 16:35:04 +0000

    Add confirmed cases per health region

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,10 +1,8 @@
 const papa = require('papaparse');
 const fs = require('fs');
 
-const graph = (infections, prediction, labels) => {
-    var ctx = document.getElementById('chart').getContext('2d');
-
-    var chart = new Chart(ctx, {
+const graph = (ctx, infections, prediction, labels) => {
+    return new Chart(ctx, {
         // The type of chart we want to create
         type: 'line',
     
@@ -42,12 +40,12 @@ const graph = (infections, prediction, labels) => {
                         borderWidth: 3,
                         borderDash: [3, 3],
                         label: {
-                            content: "Declarado estado de emergência",
+                            content: "Estado de emergência",
                             enabled: true,
                             fontFamily: 'Sen',
                             backgroundColor: '#233142',
-                            position: 'bottom',
-                            yAdjust: 15
+                            position: 'top',
+                            yAdjust: 30
                         }
                     }
                 ]
@@ -94,8 +92,7 @@ let raw_data = fs.readFileSync('covid19pt-data/data.csv', 'utf-8');
 let data = papa.parse(raw_data).data;
 
 data = data.slice(6, data.length - 1);
-
-const infections = data.map(arr => parseInt(arr[2]));
+let infections = data.map(arr => parseInt(arr[2]));
 
 let infectionRate = infections.map((_, i) => {
     return i !== infections.length - 1 ? infections.slice(i, i+2).reduce((a, b) => {
@@ -127,4 +124,29 @@ const labels = Array.apply(null, Array(infections.length + 6)).map((e, i) => {
 document.getElementById('ep_factor_value').innerHTML = `${infectionRate[Math.round(infectionRate.length - 1)]}`;
 document.getElementById('ep_factor_day').innerHTML = labels[infections.length - 1]
 
-graph(infections, prediction, labels);
+var ctx = document.getElementById('chart').getContext('2d');
+let chart = graph(ctx, infections, prediction, labels);
+
+document.getElementById('region').onchange = () => {
+    const element = document.getElementById('region');
+    chart.data.datasets[0]['data'] = data.map(arr => parseInt(arr[element.selectedIndex + 2]));
+
+    if (chart.data.datasets.length > 1) {
+        if (element.value !== 'Geral') {
+            chart.data.datasets.pop();
+        }
+    } else {
+        if (element.value == 'Geral') {
+            chart.data.datasets.push({
+                label: 'Previsão',
+                borderColor: '#E3E3E3',
+                data: prediction,
+                borderWidth: 2,
+                borderDash: [10, 5],
+                fill: false
+            })
+        }
+    }
+
+    chart.update();
+}
